41de0c078c58e4ac809ec856569c7edd4c8f0e5f
rishabhjain <rishabhjain2795@gmail.com>
2014-08-03 12:32:59 +0300

    unblock method added

--- lib/snapchat.rb
+++ lib/snapchat.rb
@@ -5,13 +5,22 @@ require "thor"
 module Snapchat
   # Your code goes here...
   class CLI < Thor
-  desc 'block', 'Block a person'
+  desc 'block', 'Block user'
   def block(input)
   	username = ask 'Enter your username:'
     password = ask 'Enter your password:'
     snapcat = Snapcat::Client.new(username)
     snapcat.login(password)
     	snapcat.block(input)
+    snapcat.logout 
+  end
+  desc 'unblock', 'Unblock user'
+  def unblock(input)
+    username = ask 'Enter your username: '
+    password = ask 'Enter your password: '
+    snapcat = Snapcat::Client.new(username)
+    snapcat.login(password)
+      snapcat.unblock(input)
     snapcat.logout 
   end
   end
